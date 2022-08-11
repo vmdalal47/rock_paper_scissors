8e7f793e7e3ce0ac984642ae80ccadc4fe9f4605
Vihar Dalal <vmdalal47@gmail.com>
2022-08-10 20:51:03 -0400

    modified app.js and index.html to add lizard and spock to game

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
+const lizard_div = document.getElementById("lizard");
+const spock_div = document.getElementById("spock");
 
 function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
-  const randomNumber = Math.floor(Math.random() * 3);
+  const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+  const randomNumber = Math.floor(Math.random() * 5);
   return choices[randomNumber];
 }
 
@@ -18,6 +20,8 @@ function convertToWord(letter) {
   if (letter === "rock") return "Rock";
   if (letter === "paper") return "Paper";
   if (letter === "scissors") return "Scissors";
+  if (letter === "lizard") return "Lizard";
+  if (letter === "spock") return "Spock";
 }
 
 function win(userChoice, computerChoice) {
@@ -59,16 +63,32 @@ function game(userChoice) {
     case "rockscissors":
     case "paperrock":
     case "scissorspaper":
+    case "rocklizard":
+    case "lizardspock":
+    case "spockscissors":
+    case "scissorslizard":
+    case "lizardpaper":
+    case "paperspock":
+    case "spockrock":
       win(userChoice, computerChoice);
       break;
     case "rockpaper":
     case "paperscissors":
     case "scissorsrock":
+    case "lizardrock":
+    case "spocklizard":
+    case "scissorsspock":
+    case "lizardscissors":
+    case "paperlizard":
+    case "spockpaper":
+    case "rockspock":
       lose(userChoice, computerChoice);
       break;
     case "rockrock":
     case "paperpaper":
     case "scissorsscissors":
+    case "lizardlizard":
+    case "spockspock":
       draw(userChoice, computerChoice);
       break;
   }
@@ -85,6 +105,14 @@ function main() {
 
   scissors_div.addEventListener('click', function() {
     game("scissors");
+  })
+
+  lizard_div.addEventListener('click', function() {
+    game("lizard");
+
+  spock_div.addEventListener('click', function() {
+    game("spock");
+  })
   })
 }
 
